1e08c343daf0b7099ee33368db37a1913edf84dc
EjiroOsiephri <osiephriejiro765@gmail.com>
2024-04-08 20:47:55 +0100

    Add: Finished basic usage of react-router-dom v15

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,26 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" exact />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
