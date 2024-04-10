811eb87cdf0b8d710499dae16c9f95e047cfbea1
EjiroOsiephri <osiephriejiro765@gmail.com>
2024-04-10 09:23:48 +0100

    Add: Created user-item and user-list styling

---
+++ vite-project/src/users/components/UserList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./UserList.css";
+import UserItem from "./UserItem";
+
+const UserList = (props) => {
+  if (props.items.length === 0) {
+    return <div className="center">No user Found</div>;
+  }
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            name={user.name}
+            image={user.image}
+            placeCount={user.places}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default UserList;
--- vite-project/src/users/pages/Users.jsx
+++ vite-project/src/users/pages/Users.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import UserList from "../components/UserList";
 
 const Users = () => {
-  return (
-    <>
-      <h2>Users work!</h2>
-    </>
-  );
+  const UsersItem = [
+    {
+      id: "u1",
+      name: "Ejiro",
+      places: 3,
+      image:
+        "https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU",
+    },
+  ];
+  return <UserList items={UsersItem} />;
 };
 
 export default Users;
