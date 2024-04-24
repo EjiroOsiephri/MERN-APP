6fb4a7ba87d05057753fd7d99a2e3beaacee2495
ejiro osiephri <osiephriejiro765@gmail.com>
2024-04-24 10:50:55 -0700

    Add: Created responsive map data using leaflets.js

--- vite-project/src/shared/components/UIElements/Map.jsx
+++ vite-project/src/shared/components/UIElements/Map.jsx
@@ -1,5 +1,51 @@
-const Map = (props) =>{
-    return <div className={`map ${props.className}`} style={props.style}></div>
-}
+import React, { useRef, useEffect } from "react";
 
-export default Map
+import "./Map.css";
+
+const Maps = (props) => {
+  const mapRef = useRef(null);
+  console.log(props.center);
+
+  const initializeMap = () => {
+    if (mapRef.current) {
+      const map = L.map(mapRef.current).setView(
+        [props.center.lat, props.center.lon],
+        props.zoom
+      );
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      const marker = L.marker([props.center.lat, props.center.lon]).addTo(map);
+      const circle = L.circle([props.center.lat, props.center.lon], {
+        color: "red",
+        fillColor: "#f03",
+        fillOpacity: 0.5,
+        radius: 500,
+      }).addTo(map);
+
+      const polygon = L.polygon([
+        [51.509, -0.08],
+        [51.503, -0.06],
+        [51.51, -0.047],
+      ]).addTo(map);
+    }
+  };
+
+  useEffect(() => {
+    initializeMap();
+  }, [props.center, props.zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+      id="map"
+    ></div>
+  );
+};
+
+export default Maps;
